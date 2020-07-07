5c488a6162e209b261f8771155e38174037da24e
daniel_eichinger <d.eichinger@campus.lmu.de>
2020-07-07 14:37:57 +0200

    refactored AppBar

--- lib/PositionTrainerWidgets/CustomAppBar.dart
+++ lib/PositionTrainerWidgets/CustomAppBar.dart
@@ -11,44 +11,36 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   }) : super(key: key);
 
   @override
+  Size get preferredSize => Size.fromHeight(height);
+
+  @override
   Widget build(BuildContext context) {
-    return AppBar(
-      backgroundColor: Theme.of(context).backgroundColor,
-      leading: Padding(
-        padding: const EdgeInsets.only(top: 12.0),
-        child: IconButton(
-          icon: Icon(
-            Icons.settings,
-            size: 26,
+    return Container(
+      margin: EdgeInsets.only(top: 20, left: 8, right: 8),
+      child: Row(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: 8),
+            child: IconButton(
+              icon: Icon(Icons.settings_applications, size: 35, color: Colors.red),
+              onPressed: () => scaffoldKey.currentState.openDrawer(),
+              color: Colors.grey,
+            ),
           ),
-          onPressed: () => scaffoldKey.currentState.openDrawer(),
-          color: Colors.grey,
-        ),
-      ),
-      titleSpacing: 0.0,
-      title: Padding(
-        padding: const EdgeInsets.only(top: 12.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Text('Stack Trainer',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 26,
-                  color: Colors.white,
-                )),
-            Container(
-                padding: EdgeInsets.only(left: 10),
-                child: Image.asset(
-                  'images/icons/card_icon_red.png',
-                  width: 60,
-                )),
-          ],
-        ),
+          Text('Stack Trainer',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+                color: Colors.white,
+              )),
+          Container(
+              padding: EdgeInsets.only(left: 10),
+              child: Image.asset(
+                'images/icons/card_icon_red.png',
+                width: 60,
+              )),
+        ],
       ),
     );
   }
-
-  @override
-  Size get preferredSize => Size.fromHeight(height);
 }
