4a2e21d5624ff807a7cacad016183103258260dd
daniel_eichinger <d.eichinger@campus.lmu.de>
2020-08-02 13:30:53 +0200

    adds additional two default card stacks

--- lib/models/card_stack_map.dart
+++ lib/models/card_stack_map.dart
@@ -8,13 +8,19 @@ part 'card_stack_map.g.dart';
 class CardStackMap {
   final Map<String, CardStack> stacks;
 
-  CardStackMap(s) : stacks = {...s, 'Mnemonica': CardStack(CONST.stack)};
+  CardStackMap(s)
+      : stacks = {
+          ...s,
+          'Mnemonica': CardStack(CONST.mnemonica),
+          'Aronson': CardStack(CONST.aronson),
+          'Memorandum': CardStack(CONST.memorandum)
+        };
 
-  factory CardStackMap.fromJson(Map<String, dynamic> json) => _$CardStackMapFromJson(json);
+  factory CardStackMap.fromJson(Map<String, dynamic> json) =>
+      _$CardStackMapFromJson(json);
   Map<String, dynamic> toJson() => _$CardStackMapToJson(this);
 
   void add(key, value) {
     stacks[key] = value;
   }
-
 }
